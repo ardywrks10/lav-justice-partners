97314473003512a15385da6d6b7cf4866277b5d3
Ardy Wirakusuma <ardywirakusuma16@gmail.com>
2026-06-05 20:16:36 +0800

    rename: FAQ.vue -> Faq.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     import Stats from '../components/Stats.vue';
     import Office from '../components/Office.vue';
     import Motto from '../components/Motto.vue';
-    import Faq from '../components/FAQ.vue';
+    import Faq from '../components/Faq.vue';
     import Alert from '../components/Alert.vue';
     import Reward from '../components/Reward.vue';
     import Articles from '../components/Articles.vue';
